6c357ac9a53b34b46453b6709222f2903f3e10fd
Harshitk15 <harshitk1x1@gmail.com>
2024-04-15 11:51:23 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+    images: {
+      domains: ['m.media-amazon.com', 'flipkart.com', 'indiamart.com'], // Add additional domains here
+    },
+  };
