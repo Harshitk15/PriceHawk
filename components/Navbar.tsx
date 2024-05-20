db8e49e6c45f4e67961f329ca84679750788af1a
Harshitk15 <harshitk1x1@gmail.com>
2024-05-20 16:17:22 +0530

    Updated Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
+const navIcons = [
+  { src: 'https://www.svgrepo.com/show/497323/money-send.svg', alt: 'pennywise' },
+]
+
 
 const Navbar = () => {
   return (
@@ -18,6 +22,23 @@ const Navbar = () => {
             Price<span className='text-primary'>Hawk</span>
           </p>
         </Link>
+
+        <div className="flex items-center gap-5">
+          {navIcons.map((icon) => (
+          <Link href="https://pennywise-zeta.vercel.app" className="flex items-center gap-1">
+            <Image 
+              key={icon.alt}
+              src={icon.src}
+              alt={icon.alt}
+              width={28}
+              height={28}
+              className="object-contain"
+            />
+            </Link>
+          ))}
+         
+        </div>
+
       </nav>
     </header>
   )
